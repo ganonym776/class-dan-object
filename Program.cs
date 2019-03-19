ac299b5487bd3ae76026d52f505675053e7695d7
Gunawan <gunawan@outlook.com>
2019-03-19 12:06:52 +0700

    commit1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mahasiswa baru = new mahasiswa();
+
+            Console.Write("Masukkan Nama\t: ");
+            baru.Nama = Console.ReadLine();
+            Console.Write("Masukkan NIM \t: ");
+            baru.Nim = Console.ReadLine();
+            Console.Write("Masukkan IPK\t: ");
+            baru.Ipk = float.Parse(Console.ReadLine());
+
+            baru.Registrasi();
+            baru.IsiKrs();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ mahasiswa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @class
+{
+    class mahasiswa
+{
+        //properties
+        public string Nim { get; set;}
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+
+        //method
+        public void Registrasi()
+        {
+            Console.WriteLine("\n\nNama\t: {0}", Nama);
+            Console.WriteLine("NIM\t: {0}", Nim);
+            Console.WriteLine("IPK\t: {0}", Ipk);
+            Console.WriteLine("\n\nTelah Melakukan Registrasi");
+        }
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} Sedang Melakukan Proses KRS\n", Nama);
+        }
+    }
+}
